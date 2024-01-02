0db1bec35509a832e2f63ce65df4bb2edd62a2fb
creatingweb123 <71700107+creatingweb123@users.noreply.github.com>
2024-01-02 11:27:25 +0900

    max route tree bfs

--- App3.java
+++ App3.java
@@ -1,66 +1,69 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 
 public class App3 {
 
-    public static class Pair{
-        int x,y;
-        Pair(int x, int y){
-            this.x = x;
-            this.y = y;
+    public static class Node{
+        int idx,dist;
+        Node(int idx, int dist){
+            this.idx = idx;
+            this.dist = dist;
         }
     }
-    public static int dfs(int[][] mapList,int[][]visited, Pair s, int fx, int fy){
-        int x,y;
-        x = s.x;  y=s.y;
-        if(x==fx&&y==fy){return 1;}
-        
-        if(visited[x][y]==-1){
-            visited[x][y] = 0;
-        }else if(visited[x][y]==0){
-            return 0;
-        }else{
-            return visited[x][y];
+    public static int[] bfs(List<Node>[]list, int start){
+        Queue<Integer> queue = new LinkedList<>();
+        int[] idxList    = new int[list.length];
+        boolean[] visited= new boolean[list.length];
+        visited[start] = true;
+        queue.add(start);
+        while(!queue.isEmpty()){
+            int x = queue.poll();
+            for(Node node:list[x]){
+                if(!visited[node.idx]){
+                    idxList[node.idx] = idxList[x]+node.dist;
+                    visited[node.idx] = true;
+                    queue.add(node.idx);
+                }
+            }
         }
-        
-        if(mapList[x+1][y]<mapList[x][y]){
-            visited[x][y] += dfs(mapList,visited,new Pair(x+1,y),fx,fy);
-        }if(mapList[x-1][y]<mapList[x][y]){
-            visited[x][y] += dfs(mapList,visited,new Pair(x-1,y),fx,fy);
-        }if(mapList[x][y+1]<mapList[x][y]){
-            visited[x][y] += dfs(mapList,visited,new Pair(x,y+1),fx,fy);
-        }if(mapList[x][y-1]<mapList[x][y]){
-            visited[x][y] += dfs(mapList,visited,new Pair(x,y-1),fx,fy);
-        }
-        return visited[x][y];
+        return idxList;
     }
 
     public static void main(String[] args) throws IOException{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
-        String[] size = bf.readLine().split(" ");
-        int x = Integer.parseInt(size[0]);
-        int y = Integer.parseInt(size[1]);
-        String[] row;
-        int[][] map  = new int[x+2][y+2];
-        int[][]visited= new int[x+2][y+2];
-        for(int i=1;i<=x;i++){
-            map[i][0]  = 10001;
-            map[i][y+1]= 10001;
-        }
-        for(int i=1;i<=y;i++){
-            map[0][i]  = 10001;
-            map[x+1][i]= 10001;
-        }
-        for(int i=1;i<=x;i++){
-            row = bf.readLine().split(" ");
-            for(int j=1;j<=y;j++){
-                visited[i][j] = -1;
-                map[i][j] = Integer.parseInt(row[j-1]);
+        int vertexNum = Integer.parseInt(bf.readLine());
+        int vertex, idx, dist;
+        List<Node>[] list = new ArrayList[vertexNum+1];
+        for(int i=1;i<=vertexNum;i++){
+            String[] vertexData = bf.readLine().split(" ");
+            vertex = Integer.parseInt(vertexData[0]);
+            list[vertex]=new ArrayList<>();
+            for(int j=1;j<vertexData.length-1;j+=2){
+                idx  = Integer.parseInt(vertexData[j]);
+                dist = Integer.parseInt(vertexData[j+1]);
+                list[vertex].add(new Node(idx,dist));
             }
         }
-        int value = dfs(map,visited,new Pair(1,1),x,y);
-        System.out.println(value);
+        int[] firstList = bfs(list,1);
+        int maxIndex = 1;
+        int maxValue = 0;
+        for(int index=1;index<firstList.length;index++){
+            if(firstList[index]>maxValue){
+                maxIndex = index;
+                maxValue = firstList[index];
+            }
+        }
+        int[] secondList = bfs(list,maxIndex);
+        maxValue = 0;
+        for(int value:secondList){
+            maxValue = (value>maxValue) ? value : maxValue;
+        }
+        System.out.println(maxValue);
     }
 }
