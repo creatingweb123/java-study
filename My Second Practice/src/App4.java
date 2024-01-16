52cb7ac99eee4cc9e00c8a26969f00ad501629cb
creatingweb123 <shegegegey1@gmail.com>
2024-01-16 16:06:52 +0900

    backjoon simple stair

--- My Second Practice/src/App4.java
+++ My Second Practice/src/App4.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+// 백준 10844
 public class App4 {
     public static void main(String[] args) throws IOException{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
