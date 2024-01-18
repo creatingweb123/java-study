2e88c7927eb83694e94ac036c33d3dd9c942a78e
creatingweb123 <shegegegey1@gmail.com>
2024-01-18 17:37:22 +0900

    backjoon backpack weight calculate

--- My Second Practice/src/App7.java
+++ My Second Practice/src/App7.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// 백준 12865
 public class App7 {
     public static void main(String[] args) throws IOException{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
