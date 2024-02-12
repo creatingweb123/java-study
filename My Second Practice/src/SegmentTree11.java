2d3ac0f8a5e763bbe8ec3388326499070948d0ff
creatingweb123 <shegegegey1@gmail.com>
2024-02-12 19:03:20 +0900

    backjoon merge segment int[]

--- My Second Practice/src/SegmentTree11.java
+++ My Second Practice/src/SegmentTree11.java
@@ -1,18 +1,13 @@
-
-// 백준 12844
-// segment tree with lazy propagation
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class SegmentTree11 {
 	public static int[] datas;
-	public static List<Integer>[] tree;
+	public static int[][] tree;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,7 +18,7 @@ public class SegmentTree11 {
 		int M = Integer.parseInt(br.readLine());
 
 		datas = new int[N + 1];
-		tree = new ArrayList[N * 4];
+		tree = new int[N * 4][];
 
 		for (int i = 1; i <= N; i++) {
 			datas[i] = Integer.parseInt(st.nextToken());
@@ -36,71 +31,58 @@ public class SegmentTree11 {
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
-			List<Integer> d = find(1, N, 1, a, b);
-			int value = upperBound(d, c) + 1;
-			sb.append((d.size() - value) + "\n");
+			int[] d = find(1, N, 1, a, b);
+			int value = upperBound(d, c);
+			sb.append((d.length - value) + "\n");
 		}
 		bw.write(sb.toString());
 		bw.flush();
 		bw.close();
 	}
 
-	public static List<Integer> init(int start, int end, int node) {
+	public static int[] init(int start, int end, int node) {
 		if (start == end) {
-			tree[node] = new ArrayList<>();
-			tree[node].add(datas[start]);
-			return tree[node];
+			return tree[node] = new int[] { datas[start] };
 		}
 		int mid = (start + end) / 2;
 		return tree[node] = merge(init(start, mid, node * 2), init(mid + 1, end, node * 2 + 1));
 	}
 
-	public static List<Integer> merge(List<Integer> a, List<Integer> b) {
+	public static int[] merge(int[] a, int[] b) {
 		int aStart = 0;
 		int bStart = 0;
 
-		int aLength = a.size();
-		int bLength = b.size();
-		if (aLength == 0)
-			return b;
-		if (bLength == 0)
-			return a;
-
-		List<Integer> c = new ArrayList<>();
-		for (; aStart < aLength && bStart < bLength;) {
-			if (a.get(aStart) < b.get(bStart)) {
-				c.add(a.get(aStart++));
+		int aLength = a.length;
+		int bLength = b.length;
+		int[] c = new int[aLength + bLength];
+		for (int i = 0; i < c.length; i++) {
+			if (aStart < aLength && (bStart >= bLength || a[aStart] < b[bStart])) {
+				c[i] = a[aStart++];
 			} else {
-				c.add(b.get(bStart++));
+				c[i] = b[bStart++];
 			}
-		}
-		for (; aStart < aLength;) {
-			c.add(a.get(aStart++));
-		}
-		for (; bStart < bLength;) {
-			c.add(b.get(bStart++));
 		}
 		return c;
 	}
 
-	public static List<Integer> find(int start, int end, int node, int left, int right) {
+	public static int[] find(int start, int end, int node, int left, int right) {
 		if (right < start || end < left) {
-			return new ArrayList<>();
+			return new int[0];
 		}
 		if (left <= start && end <= right) {
-			return tree[node];
+			return new int[] { tree[node] };
 		}
 		int mid = (start + end) / 2;
 		return merge(find(start, mid, node * 2, left, right), find(mid + 1, end, node * 2 + 1, left, right));
 	}
 
-	public static int upperBound(List<Integer> a, int key) {
+	public static int upperBound(int[] a, int key) {
 		int front = 0;
-		int rear = a.size() - 1;
+		int rear = a.length - 1;
 		int mid;
 		while (front < rear) {
 			mid = (front + rear) / 2;
-			if (a.get(mid) < key)
+			if (a[mid] <= key)
 				front = mid + 1;
 			else
 				rear = mid;
